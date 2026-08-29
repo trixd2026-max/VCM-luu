0e7812d9a54dce06534d00396c9b9fd74565293a
trixd2026-max <trixd2026@gmail.com>
2026-08-29 10:34:22 +0700

    Sửa màu icon Facebook header sang xanh chuẩn #1877F2

--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -154,7 +154,7 @@ function LeafMark() {
 
 function FacebookMark() {
   return (
-    <span className="grid size-7 place-items-center rounded-md bg-facebook text-white">
+    <span className="grid size-7 place-items-center rounded-md bg-[#1877F2] text-white">
       <svg viewBox="0 0 24 24" className="size-4 fill-current" aria-hidden>
         <path d="M12 2.04c-5.5 0-10 4.49-10 10.02 0 5 3.66 9.15 8.44 9.9v-7H7.9v-2.9h2.54V9.85c0-2.52 1.49-3.91 3.78-3.91 1.09 0 2.23.2 2.23.2v2.46h-1.26c-1.24 0-1.63.77-1.63 1.56v1.88h2.78l-.44 2.9h-2.34v7c4.78-.75 8.44-4.9 8.44-9.9 0-5.53-4.5-10.02-10-10.02Z" />
       </svg>
